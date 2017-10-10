0a886198b72f5418b377a69ffa44a3f1f1b62dc0
programmiertier <zimmermannveronique@protonmail.com>
2017-10-10 09:36:12 +0200

    Datum und Zeit
    Positionierung.
    Datum und Zeit ausgeben

--- Formatausdruck_Console/Program.cs
+++ Formatausdruck_Console/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
+using static System.ConsoleColor;
 
 namespace Formatausdruck_Console
 {
@@ -10,6 +8,24 @@ namespace Formatausdruck_Console
     {
         static void Main(string[] args)
         {
+            int maxWidth = LargestWindowWidth;
+            int maxHeight = LargestWindowHeight;
+            int topLine = 0;
+            SetWindowSize(maxWidth / 2, maxHeight / 2);
+            BackgroundColor = Black;
+            Clear();
+            string dateString = $"Heute: {DateTime.Now.ToLongDateString()}";
+            string zeitString = $"Jetzt: {DateTime.Now.ToShortTimeString()}";    // ToLongtTimeString, zeigt bis zur Sekunde
+            int lengthDateTimeNow = dateString.Length;
+            int lengthzeitString = zeitString.Length;
+
+            SetCursorPosition(maxWidth / 2 - lengthDateTimeNow - 1, topLine);
+            ForegroundColor = Red;
+            WriteLine(dateString);
+
+            ForegroundColor = Green;
+            SetCursorPosition(maxWidth / 2 - lengthzeitString - 1, ++topLine);
+            WriteLine(zeitString);
         }
     }
 }
